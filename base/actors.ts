8be6b6b23666ce71c599190c8ef664c8bafa733d
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-12-06 23:34:37 +0100

    Implement RNG and HasOpinions
    not having Python operator overrides' auto-reflexivity will make implementation harder, but so be it

--- base/actors.ts
+++ base/actors.ts
@@ -1,4 +1,5 @@
 import { math, range, sum } from "../utils/python";
+import RNG from "../utils/RNG";
 
 const SQ2 = Math.sqrt(2);
 
@@ -17,6 +18,10 @@ function normalToUniform(x: number, mu: number, sigma: number): number {
     return 0.5 * (1 + math.erf((x - mu) / (sigma * SQ2)));
 }
 
+export function getDefaultAlignmentFactors(nopinions: number) {
+    return range(nopinions).map(i => 1 - i / nopinions);
+}
+
 /**
  * Returns the one-dimensional alignment of the opinions.
  *
@@ -31,25 +36,111 @@ function normalToUniform(x: number, mu: number, sigma: number): number {
  * @param factors ponderation for each opinion's importance in the alignment
  * @returns
  */
-export function get_alignment(
+function getAlignment(
     opinions: number[],
     opinmax: number,
-    factors?: number[],
+    factors: number[] = getDefaultAlignmentFactors(opinions.length),
 ): number {
-    const nopinions = opinions.length;
-    if (factors === undefined) {
-        factors = [];
-        for (let i = 0; i < nopinions; i++) {
-            factors.push(1-i/nopinions);
-        }
-    }
-
     const scapro = sum(opinions.map((opinion, i) => opinion * factors[i]));
     const rang = range(-opinmax, opinmax + 1);
     // standard deviation of one opinion taken on its own
-    const one_sigma = Math.sqrt(sum(rang.map(i => i**2)) / rang.length);
+    const one_sigma = Math.sqrt(sum(rang.map(i => i ** 2)) / rang.length);
     // using Lyapunov's central limit theorem
     const sigma = math.hypot(...factors.map(f => f * one_sigma));
 
     return normalToUniform(scapro, 0, sigma);
+}
+
+/**
+ * An abstract base class for objects that have or represent opinions on subjects.
+ * This typically covers voters, parties, and bills.
+ *
+ * The nopinions, opinmax, and opinionAlignmentFactors static attributes
+ * should be global to a given system, and be set once in an abstract subclass
+ * of HasOpinion that will be a base class for all your classes having opinions.
+ * In any case, HasOpinions subclasses should definitely not support operations
+ * between subclasses with different nopinions or opinmax values.
+ *
+ * The opinions of someone or something are represented as a multi-dimensional vector
+ * of integers, with each dimension representing one subject on which an opinion is held.
+ * The value in each dimension can be symmetrically positive or negative.
+ * Values close to 0 represent neutrality or indecision. There are nopinions dimensions,
+ * and each dimension can take values between -opinmax and opinmax (included).
+ */
+export abstract class HasOpinions {
+    static nopinions: number;
+    static opinmax: number;
+    static opinionAlignmentFactors?: number[];
+
+    opinions: number[];
+
+    /**
+     * If no opinion sequence is provided, the opinions are generated following an
+     * integral uniform law for each dimension. The optional randomObj parameter
+     * can be used to provide a random number generator (a RNG instance) to generate
+     * the opinions, or the optional randomSeed parameter can be used to deterministically
+     * seed a new RNG instance.
+     */
+    constructor();
+    constructor({ opinions }: { opinions: number[] });
+    constructor({ randomObj }: { randomObj: RNG });
+    constructor({ randomSeed }: { randomSeed: number | string });
+    constructor({ opinions, randomObj, randomSeed }:
+        { opinions?: number[], randomObj?: RNG, randomSeed?: number | string } = {},
+    ) {
+        const cls = this.constructor as typeof HasOpinions;
+        if (opinions === undefined) {
+            if (randomObj === undefined) {
+                randomObj = new RNG(randomSeed);
+            }
+            opinions = randomObj.choices(range(-cls.opinmax, cls.opinmax + 1), cls.nopinions);
+        }
+        this.opinions = opinions;
+    }
+
+    /**
+     * The objects having an opinion (typically, voters and parties) can be
+     * placed on a single-dimension axis (typically left-wing to right-wing),
+     * called the alignment.
+     * The opinionAlignmentFactors static attribute provides a sequence of
+     * ponderation factors for each dimentsion of the opinion vector. If it is
+     * not provided, the default one is generated from the number of opinions
+     * by the getDefaultAlignmentFactors function.
+     */
+    get alignment(): number {
+        const cls = this.constructor as typeof HasOpinions;
+        return this.getAlignment(cls.opinionAlignmentFactors);
+    }
+
+    /**
+     * This is similar to the alignment getter, but allowing a custom factors
+     * sequence to be provided.
+     * (Not sure about making it public.)
+     */
+    private getAlignment(factors?: number[]): number {
+        const cls = this.constructor as typeof HasOpinions;
+        return getAlignment(this.opinions, cls.opinmax, factors);
+    }
+
+    /**
+     * Subclasses of HasOpinions may override this method. It should take
+     * instances of a subclass of HasOpinions with the same nopinions and opinmax
+     * values, but not necessarily the same class as the "this" object,
+     * and return a value proportional to the disagreement between the two objects
+     * (the higher the value, the stronger the disagreement).
+     *
+     * Not all (concrete) subclasses need to support disagreement with all other
+     * subclasses in the same system, or even with other instances of the same subclass,
+     * but the voting methods rely on disagreement being supported at least between the
+     * voter class and the party class.
+     *
+     * The operation should be symmetric : a.disagree(b) should be the same as b.disagree(a).
+     *
+     * However, the operation does not need to be symmetric when it comes to types :
+     * a given object o of type O can have different disagreement values with objects
+     * of types P and Q having the same opinions array.
+     * Similarly, a party with opinions o1 and a voter with opinions o2 can have a different
+     * disagreement value than a voter with opinions o1 and a party with opinions o2.
+     */
+    abstract disagree<HO extends HasOpinions>(other: HO): number;
 }
---
+++ utils/RNG.ts
@@ -0,0 +1,76 @@
+/**
+ * Deterministic and reseedable RNG with utility methods
+ */
+export default class RNG {
+    private readonly m: number;
+    private readonly a: number;
+    private readonly c: number;
+    private state!: number;
+    /**
+     * @param seed integer between 0 and 2^31 - 1 ; if not passed, generates it from Math.random()
+     */
+    constructor(seed?: number|string) {
+        // LCG using GCC's constants
+        this.m = 0x80000000; // 2**31;
+        this.a = 1103515245;
+        this.c = 12345;
+
+        this.seed = seed;
+    }
+    /**
+     * @param seed integer between 0 and 2^31 - 1 ; if undefined, generates it from Math.random()
+     */
+    set seed(seed: number|string|undefined) {
+        if (seed === undefined) {
+            this.state = Math.floor(Math.random() * (this.m - 1));
+        } else {
+            if (typeof seed === "string") {
+                seed = hashString(seed);
+            }
+            this.state = Math.abs(seed);
+        }
+    }
+    /**
+     * @returns a number presumably in [[0, 2**31[[
+     */
+    randInt() {
+        this.state = (this.a * this.state + this.c) % this.m;
+        return this.state;
+    }
+    /**
+     * @returns a number in [0, 1[
+     */
+    random() {
+        return this.randInt() / this.m;
+    }
+    /**
+     * @returns a number in [[min, max[[
+     */
+    randRange(min: number, max: number) {
+        return min + Math.floor(this.random() * (max - min));
+    }
+    /**
+     * @returns one of the elements
+     */
+    choice<T>(array: T[]) {
+        return array[this.randRange(0, array.length)];
+    }
+    /**
+     * Picks k elements from the array without replacement.
+     * This reseeds k times, not 1 time.
+     * @param k number of elements to choose
+     */
+    choices<T>(array: T[], k: number): T[] {
+        const copy = array.slice();
+        return Array(k).map(() => copy.splice(this.randRange(0, copy.length), 1)[0]);
+    }
+}
+
+function hashString(str: string): number {
+    let hash = 0;
+    for (let i = 0; i<str.length; i++) {
+        hash = ((hash << 5) - hash) + str.charCodeAt(i);
+        hash |= 0;
+    }
+    return hash;
+}
