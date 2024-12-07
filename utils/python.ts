b8a6aef4dbc5ff7e0c071a0e1510854d8ade0866
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-12-07 16:22:38 +0100

    Add more builtins

--- utils/python.ts
+++ utils/python.ts
@@ -14,3 +14,35 @@ export function range(start: number, end?: number): number[] {
         return Array.from({ length: end - start }, (_, i) => i + start);
     }
 }
+
+export function* enumerate<T>(array: Iterable<T>, start=0) {
+    for (const item of array) {
+        yield [start++, item] as const;
+    }
+}
+
+export function max<T>(array: Iterable<number>): number;
+export function max<T>(array: Iterable<T>, key: (n: T) => number): T;
+export function max(array: Iterable<any>, key?: (n: any) => number): any {
+    if (!(array instanceof Array)) {
+        return max([...array], key);
+    }
+    if (key === undefined) {
+        return array.reduce((a, b) => a > b ? a : b);
+    } else {
+        return array.reduce((a, b) => key(a) > key(b) ? a : b);
+    }
+}
+
+export function min<T>(array: Iterable<number>): number;
+export function min<T>(array: Iterable<T>, key: (n: T) => number): T;
+export function min(array: Iterable<any>, key?: (n: any) => number): any {
+    if (!(array instanceof Array)) {
+        return min([...array], key);
+    }
+    if (key === undefined) {
+        return array.reduce((a, b) => a < b ? a : b);
+    } else {
+        return array.reduce((a, b) => key(a) < key(b) ? a : b);
+    }
+}
