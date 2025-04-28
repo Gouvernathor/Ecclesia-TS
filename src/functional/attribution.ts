e57ad2d4a4e82126d6d198423af3fe60d2603c36
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-04-28 22:44:31 +0200

    Add documentation

--- src/functional/attribution.ts
+++ src/functional/attribution.ts
@@ -1,12 +1,41 @@
 import { Counter } from "@gouvernathor/python/collections";
 import { Ballots, Simple } from "../ballots";
 
+/**
+ * To be thrown when an attribution fails to attribute seats,
+ * but only in a case where it's an expected limitation of the attribution method.
+ * The typical example being the Condorcet standoff, or a majority threshold,
+ * but a tie could be another example (though they are usually not checked in provided implementations).
+ *
+ * Other errors may and will be raised by the attribution methods for other reasons,
+ * such as invalid input data, division by zero...
+ */
 export class AttributionFailure extends Error { }
 
-// in doc, recommend throwing AttributionFailure
+/**
+ * A function to manage how the results from the ballots translate into an allocation of seats.
+ *
+ * If the attribution method is expected, by design, to fail under expected conditions,
+ * such as a majority threshold not being reached,
+ * this method should raise an AttributionFailure error.
+ *
+ * @param rest This parameter, ignored in most cases,
+ * forces the attributions taking more required or optional parameters
+ * to take them as an options object.
+ *
+ * @returns The attribution of seats to the parties based upon the votes.
+ * It is a Counter mapping each party to the number of seats it won.
+ * The return value's total() should be equal to the nSeats attributes - if any.
+ */
 export type Attribution<Party, B extends Ballots<Party>> =
     (votes: B, rest?: Record<string, any>) => Counter<Party>;
 
+/**
+ * Most attributions should generally implement this interface, and have a fixed number of seats.
+ *
+ * However, some attributions may yield variable numbers of seats,
+ * for instance the pre-2024 federal German system.
+ */
 export type HasNSeats = { readonly nSeats: number };
 
 
@@ -16,15 +45,25 @@ partial factory methods
  */
 
 /**
- * Stands in for the Proportional class implementation.
+ * Transforms a standard proportional attribution method into one that requires a certain threshold
+ * (percentage of the total votes) to be reached by the parties in order to receive seats.
  *
- * The votes passed to the proportionalAttrib method are guaranteed to be above the threshold,
- * except that if no party reached the threshold, and the contingency is undefined (which is not the same as null),
- * all votes will be passed.
+ * Replaces the Proportional class implementation.
+ *
+ * @param threshold The threshold, between 0 and 1
+ * @param contingency The fallback attribution in case the threshold is not reached by any party.
+ * It takes an Attribution, or undefined (the default), or null.
+ * Null in this case has the specific behavior that if no party reaches the threshold,
+ * an AttributionFailure error will be raised.
+ * If undefined (or nothing is passed), the contingency will default to running
+ * the same attribution method without the threshold.
+ * @param proportionalAttrib The votes passed to this attribution method are guaranteed to be above the threshold,
+ * except if the contingency is undefined and no party reached the threshold
+ * (in that case, all parties will be passed).
  */
 export function addThresholdToProportional<Party>(
-    { threshold = 0, contingency, proportionalAttrib }: {
-        threshold?: number,
+    { threshold, contingency, proportionalAttrib }: {
+        threshold: number,
         contingency?: Attribution<Party, Simple<Party>> | null,
         proportionalAttrib: Attribution<Party, Simple<Party>>,
     }
@@ -59,13 +98,32 @@ export function addThresholdToProportional<Party>(
     return attrib;
 }
 
+/**
+ * A function that should be pure : it should not take into account
+ * any value other than the arguments passed to it.
+ *
+ * @param t The fraction of votes received by a candidate
+ * @param a The number of seats already allocated to that candidate
+ * @returns A value that should be increasing as t rises, and decreasing as a rises.
+ * The higher the return value, the more likely the candidate is to receive another seat.
+ */
+export type RankIndexFunction = (t: number, a: number) => number;
+
+/**
+ * A function creating a fixed-seats, proportional, rank-index attribution method
+ * from a rank-index function.
+ *
+ * The implementation is optimized so as to call rankIndexFunction as few times as possible.
+ *
+ * Replaces the RankIndexMethod class implementation.
+ */
 export function proportionalFromRankIndexFunction<Party>(
     { nSeats, rankIndexFunction }: {
         nSeats: number,
-        rankIndexFunction: (t: number, a: number) => number,
+        rankIndexFunction: RankIndexFunction,
     }
-): Attribution<Party, Simple<Party>> {
-    return (votes: Simple<Party>, rest = {}): Counter<Party> => {
+): Attribution<Party, Simple<Party>> & HasNSeats {
+    const attrib = (votes: Simple<Party>, rest = {}): Counter<Party> => {
         const allVotes = votes.total;
         const fractions = new Map([...votes.entries()].map(([party, v]) => [party, v / allVotes]));
 
@@ -90,14 +148,27 @@ export function proportionalFromRankIndexFunction<Party>(
 
         return seats;
     }
+    attrib.nSeats = nSeats;
+    return attrib;
 }
 
+/**
+ * A function that should be pure.
+ * @param k The number of seats already allocated to a party
+ * @returns A value that should be increasing as k rises
+ */
+type DivisorFunction = (k: number) => number;
+
+/**
+ * A function creating a divisor method -
+ * one kind of rank-index attribution, itself a kind of proportional attribution.
+ */
 export function proportionalFromDivisorFunction<Party>(
     { nSeats, divisorFunction }: {
         nSeats: number,
-        divisorFunction: (k: number) => number,
+        divisorFunction: DivisorFunction,
     }
-): Attribution<Party, Simple<Party>> {
+): Attribution<Party, Simple<Party>> & HasNSeats {
     return proportionalFromRankIndexFunction({
         nSeats,
         rankIndexFunction: (t, a) => t / divisorFunction(a),
