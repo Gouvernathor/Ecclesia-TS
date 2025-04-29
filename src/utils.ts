ce7ac6fa22a1b10251339ac1df111d529cd4d5f3
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-04-29 11:22:30 +0200

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import RNG from "@gouvernathor/rng";
 
-export type RandomObjParam = undefined | {randomObj: RNG} | {randomSeed: number|string};
+export type RandomObjParam = undefined | {randomObj: RNG} | {randomSeed?: number|string};
 
 export function createRandomObj(param: RandomObjParam): RNG;
 export function createRandomObj({ randomObj, randomSeed }:
