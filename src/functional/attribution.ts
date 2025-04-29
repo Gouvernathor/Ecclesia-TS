cd4f0edd0a634d8b9e3bca011e2651e6863d5d0a
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-04-29 02:23:22 +0200

    Fix huntington-hill implementation

--- src/functional/attribution.ts
+++ src/functional/attribution.ts
@@ -526,14 +526,55 @@ export function hare<Party>(
 export const largestRemaindersProportional = hare;
 export const hamilton = hare;
 
-// FIXME
+/**
+ * This attribution method required some creativity and tweaks,
+ * since the standard divisor won't work without an initial seats value,
+ * causing a division by 0.
+ * As a result, a threshold is required in this case.
+ *
+ * In a situation where the candidates are already limited by other means,
+ * (for instance when distributing representatives between a fixed number of states)
+ * to be less or equal to the number of seats, then a threhold of 0 can be passed.
+ * In that case, the method will allocate one seat to each candidate
+ * until all candidates have a seat,
+ * or (but that would be a bug) all seats are allocated.
+ *
+ * The order in which the first seats are allocated is an implementation detail.
+ */
+export function huntingtonHill<Party>(
+    { nSeats, threshold }: {
+        nSeats: number,
+        threshold: 0,
+    }
+): DivisorMethod<Party> & HasNSeats;
+export function huntingtonHill<Party>(
+    { nSeats, threshold, contingency }: {
+        nSeats: number,
+        threshold: number,
+        contingency?: DivisorMethod<Party> | null,
+    }
+): DivisorMethod<Party> & HasNSeats;
+export function huntingtonHill<Party>(
+    { nSeats, threshold, contingency }: {
+        nSeats: number,
+        threshold: number,
+        contingency?: RankIndexMethod<Party> | null,
+    }
+): RankIndexMethod<Party> & HasNSeats;
+export function huntingtonHill<Party>(
+    { nSeats, threshold, contingency }: {
+        nSeats: number,
+        threshold: number,
+        contingency?: Attribution<Party, Simple<Party>> | null,
+    }
+): Attribution<Party, Simple<Party>> & HasNSeats;
 export function huntingtonHill<Party>(
     { nSeats, threshold, contingency = null }: {
         nSeats: number,
         threshold: number,
         contingency?: Attribution<Party, Simple<Party>> | null,
     }
-): DivisorMethod<Party> & HasNSeats {
+) {
     const divisorFunction = (k: number) => Math.sqrt(k * (k + 1));
     const baseRankIndexFunction = rankIndexFunctionFromDivisorFunction(divisorFunction);
     const rankIndexFunction = (t: number, a: number) => {
@@ -543,27 +584,16 @@ export function huntingtonHill<Party>(
         return baseRankIndexFunction(t, a);
     };
 
-    return proportionalFromRankIndexFunction({
-        nSeats,
-        rankIndexFunction,
-    });
-
-    // const attrib = (votes: Simple<Party>, rest = {}): Counter<Party> => {
-    //     const originalVotes = votes;
-    //     const thresholdVotes = threshold * votes.total;
-    //     votes = new Counter<Party>([...votes.entries()].filter(([_, score]) => score >= thresholdVotes));
-
-    //     if (votes.size === 0) {
-    //         if (contingency === null) {
-    //             throw new AttributionFailure("No party reached the threshold");
-    //         }
-    //         return contingency(originalVotes, rest);
-    //     }
-    //     return proportionalFromDivisorFunction<Party>({
-    //         nSeats,
-    //         divisorFunction,
-    //     })(votes, rest);
-    // }
+    const attrib = addThresholdToSimpleAttribution({
+        threshold,
+        contingency,
+        attribution: proportionalFromRankIndexFunction({
+            nSeats,
+            rankIndexFunction,
+        }),
+    }) as Attribution<Party, Simple<Party>> & { nSeats?: number };
+    attrib.nSeats = nSeats;
+    return attrib;
 }
 
 
