c34e1b9449e66ebc5b2bb9e9824ddb13a1705858
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-04-30 01:51:50 +0200

    Cleanup obsolete ballots

--- src/ballots.ts
+++ src/ballots.ts
@@ -32,22 +32,10 @@ export interface Order<Party> extends ReadonlyArray<ReadonlyArray<Party>> { }
  * If the voter must grade all the candidates, then sum(result.get(x)) is constant
  * and equal to the number of voters.
  *
- * If the ngrades attribut was set, any party not mapped will be assumed to have
+ * Any party not mapped will be assumed to have
  * an array of zeros (of length ngrades).
- * It is advised to either pass non-empty arguments to the default constructor
- * (which is the same as Map's), or to pass the number of grades to the fromGrades
- * alternative constructor which will build an empty instance.
- * Otherwise, the ngrades attribute will not be set and the get method may return
- * undefined for unlisted parties.
  */
-export interface MehScores<Party> extends ReadonlyMap<Party, ReadonlyArray<number>> {
-    readonly ngrades?: number;
-    get(key: Party): ReadonlyArray<number>|undefined;
-}
-/**
- * Better version of scores
- */
-export interface Scores<Party> extends MehScores<Party> {
+export interface Scores<Party> extends ReadonlyMap<Party, ReadonlyArray<number>> {
     readonly ngrades: number;
     get(key: Party): ReadonlyArray<number>;
 }
@@ -67,14 +55,14 @@ export namespace Scores {
         if (elements.length === 0) {
             throw new Error("Use the fromGrades method to create an empty Scores instance");
         }
-        const ths = new Map(elements) as MehScores<Party> & { ngrades?: number };
+        const ths = new Map(elements) as Partial<Scores<Party>> & { ngrades?: number };
         ths.ngrades = elements[0][1].length;
         ths.get = get.bind(ths as any);
         return ths as Scores<Party>;
     }
 
     export function fromGrades<Party>(ngrades: number): Scores<Party> {
-        const ths = new Map() as MehScores<Party> & { ngrades?: number };
+        const ths = new Map() as Partial<Scores<Party>> & { ngrades?: number };
         ths.ngrades = ngrades;
         ths.get = get.bind(ths as any);
         return ths as Scores<Party>;
@@ -82,5 +70,4 @@ export namespace Scores {
 }
 
 
-export type MehBallots<Party> = Simple<Party> | Order<Party> | MehScores<Party>;
 export type Ballots<Party> = Simple<Party> | Order<Party> | Scores<Party>;
