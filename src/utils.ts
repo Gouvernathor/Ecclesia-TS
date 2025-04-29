40ae7512a5719722ab8819ab4aa8f65352166f17
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-04-29 15:45:50 +0200

    Tweak RandomObjParam type

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
 import RNG from "@gouvernathor/rng";
 
-export type RandomObjParam = undefined | {randomObj: RNG} | {randomSeed?: number|string};
+export type RandomObjParam = {randomObj: RNG} | {randomSeed?: number|string};
 
-export function createRandomObj(param: RandomObjParam): RNG;
+export function createRandomObj(param?: RandomObjParam): RNG;
 export function createRandomObj({ randomObj, randomSeed }:
     { randomObj?: RNG, randomSeed?: number | string } = {},
 ): RNG {
