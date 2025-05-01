43f1b8046097543639b358c2fd651064edc6df80
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-05-02 00:16:20 +0200

    Add disproportion metrics
    not currently exported

---
+++ src/election/attribution/metrics.ts
@@ -0,0 +1,31 @@
+import { type Counter } from "@gouvernathor/python/collections";
+import { type Simple } from "../ballots";
+
+export interface DisproportionMetric<Party> {
+    (p0: {votes: Simple<Party>, seats: Counter<Party>}): number;
+}
+
+/**
+ * Returns the mean value (across all candidates) of the absolute difference
+ * between the theoretical, (f)ra(c)tional number of seats and the allocated number of seats.
+ * The bigger, the less proportional.
+ *
+ * Compared to a sum or mean of absolute differences of percentage,
+ * this
+ */
+export function defaultMetric<Party>(
+    { votes, seats }: {
+        votes: Simple<Party>,
+        seats: Counter<Party>,
+    }
+): number {
+    const allVotes = votes.total;
+    const allSeats = seats.total;
+    let suum = 0;
+    for (const party of seats.keys()) {
+        const partyVotes = votes.get(party)!;
+        const partySeats = seats.get(party)!;
+        suum += Math.abs(allSeats * partyVotes / allVotes - partySeats);
+    }
+    return suum / seats.size;
+}
