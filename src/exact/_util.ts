005f0c883b1521ad0c7ddcf8e50ad0ec49a7e9a5
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-04-10 20:21:15 +0200

    Finish voting to ballot

--- src/exact/_util.ts
+++ src/exact/_util.ts
@@ -1,9 +1,24 @@
 import { Fraction, FractionAble } from "@gouvernathor/fraction.ts";
 
-export const FRAC0: Fraction = Fraction.fromNumeric(0n);
 export const FRAC1: Fraction = Fraction.fromNumeric(1n);
 
-export function min(array: Iterable<FractionAble>): FractionAble {
+export function max<T extends FractionAble>(array: Iterable<T>): T {
+    return maxBy(array, t => t);
+}
+export function maxBy<T>(array: Iterable<T>, key: (t: T) => FractionAble): T {
+    let maxElement;
+    let maxValue: Pick<Fraction, "lt"> = { lt: () => true };
+    for (const element of array) {
+        const value = Fraction.fromAny(key(element));
+        if (maxValue.lt(value)) {
+            maxValue = value;
+            maxElement = element;
+        }
+    }
+    return maxElement!;
+}
+
+export function min<T extends FractionAble>(array: Iterable<T>): T {
     return minBy(array, t => t);
 }
 export function minBy<T>(array: Iterable<T>, key: (t: T) => FractionAble): T {
--- src/exact/election/voting-to-ballot.ts
+++ src/exact/election/voting-to-ballot.ts
@@ -1,7 +1,7 @@
 import { Fraction, FractionAble } from "@gouvernathor/fraction.ts";
-import { Ranked, Single } from "../../election/ballot";
+import { Approval, Ranked, Score, Single } from "../../election/ballot";
 import { VotingToBallot } from "../../election/voting-to-ballot";
-import { minBy } from "../_util";
+import { FRAC1, max, min, minBy } from "../_util";
 
 export interface DisagreementFunction<T, U> {
     (t: T, u: U): FractionAble;
@@ -32,4 +32,90 @@ export function orderingVote<Voter, Candidate>(
 ): VotingToBallot<Voter, Candidate, Ranked<Candidate>> {
     return (voter, candidates) =>
         Array.from(candidates).sort((a, b) => Fraction.compare(disagree(voter, a), disagree(voter, b)));
+}
+
+/**
+ * The voter gives a score (or a grade) to each candidate.
+ * The number of grades must be provided to the constructor.
+ *
+ * The implementation of this voting method is not as straightforward as the two previous ones,
+ * even setting strategic voting aside.
+ * What is to be considered to be the range of grades to cover ?
+ * From nazis to angels, or from the worst present candidate to the best ?
+ * The answer lies only in the minds of the voters.
+ * The latter is more akin to orderingVote, so I implemented the former,
+ * but it causes issues for lower grades so approvalVote uses the latter.
+ *
+ * In this implementation, the voter gives to each candidate
+ * a score proportional to the raw disagreement.
+ * This may yield situations where every party is graded 0,
+ * especially with low nScores values.
+ */
+export function cardinalVote<Voter, Candidate>(
+    { nScores, disagree }: {
+        nScores: number,
+        disagree: DisagreementFunction<Voter, Candidate>,
+    }
+): VotingToBallot<Voter, Candidate, Score<Candidate>> {
+    return (voter, candidates) =>
+        new Map(Array.from(candidates, candidate => {
+            const grade = min([
+                nScores - 1,
+                FRAC1.sub(disagree(voter, candidate)).mul(Math.floor(nScores)),
+            ]);
+            return [candidate, +grade];
+        }));
+}
+
+/**
+ * Alternative implementation of cardinalVote.
+ * The candidate(s) the least disagreed with are given the maximum score (which is nScores-1),
+ * the candidate(s) the most disagreed with (unless all candidates have the same raw disagreement) gets 0,
+ * and the score of the other candidates is proportional (well, affine) between those values.
+ */
+export function balancedCardinalVote<Voter, Candidate>(
+    { nScores, disagree }: {
+        nScores: number,
+        disagree: DisagreementFunction<Voter, Candidate>,
+    }
+): VotingToBallot<Voter, Candidate, Score<Candidate>> {
+    return (voter, candidates) => {
+        const prefs = new Map(Array.from(candidates, candidate => [candidate, FRAC1.sub(disagree(voter, candidate))]));
+        const minPref = min(prefs.values());
+        let maxPref = max(prefs.values());
+
+        if (minPref !== maxPref) { // avoid division by zero
+            maxPref = maxPref.sub(minPref);
+        }
+
+        return new Map(Array.from(candidates, candidate => {
+            const grade = min([
+                nScores - 1,
+                ((prefs.get(candidate)!.sub(minPref)).mul(nScores).div(maxPref)).floor(),
+            ]);
+            return [candidate, Number(grade)];
+        }));
+    };
+}
+
+/**
+ * The voter approves or disapproves each of the candidates.
+ *
+ * Technically a special case of a cardinal/score vote where scores are 0 and 1,
+ * but this one makes it open to additional attribution methods
+ * (proportional ones for instance).
+ * That's why the format it returns is not the same as with the cardinal vote.
+ * If you want a scores-like ballot, use balancedCardinalVote({ nScores: 2 }) instead.
+ */
+export function approvalVote<Voter, Candidate>(
+    { disagree }: {
+        disagree: DisagreementFunction<Voter, Candidate>,
+    }
+): VotingToBallot<Voter, Candidate, Approval<Candidate>> {
+    const cardinal = balancedCardinalVote({ nScores: 2, disagree });
+    return (voter, candidates) => {
+        const scores = cardinal(voter, candidates);
+        return new Set(Array.from(scores.keys())
+            .filter(candidate => scores.get(candidate)! > 0));
+    };
 }
