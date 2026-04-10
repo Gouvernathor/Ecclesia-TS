a6fda2b6e3d913748c6a55a12e089f8ac1c2092f
Gouvernathor <gouvernathor@noreply.codeberg.org>
2026-04-10 13:35:18 +0200

    Basic metric tests

---
+++ test/election/attribution/metrics.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from "vitest";
+import { NumberCounter } from "@gouvernathor/python/collections";
+import { defaultMetric } from "../../../src/election/attribution/metrics";
+
+describe("defaultMetric", () => {
+    it("returns 0 for an exact match", () => {
+        const votes = NumberCounter.fromKeys("aaabbc");
+        const seats = votes;
+        const metricResult = defaultMetric({ votes, seats });
+
+        expect(metricResult).toBe(0);
+    });
+
+    it("does not error out when a party has no votes", () => {});
+    it("does not error out when a party has no seats", () => {});
+
+    it("returns a decreasing value for increasing accuracy", () => {
+        const votes = NumberCounter.fromKeys("a".repeat(101)+"b".repeat(100)+"c".repeat(99));
+        const seats1 = NumberCounter.fromKeys("a".repeat(8)+"b".repeat(2)+"c".repeat(2));
+        const seats2 = NumberCounter.fromKeys("a".repeat(5)+"b".repeat(4)+"c".repeat(3));
+
+        const metricResult1 = defaultMetric({ votes, seats: seats1 });
+        const metricResult2 = defaultMetric({ votes, seats: seats2 });
+
+        expect(metricResult1).toBeGreaterThan(metricResult2);
+    });
+});
